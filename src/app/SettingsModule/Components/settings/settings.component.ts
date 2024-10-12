8581aebb636c7fcfb87f06ac14d3f7bc91dd327f
Luan Oak <luancarvprofissional@gmail.com>
2024-10-12 16:46:50 -0300

    settings frontend

--- src/app/SettingsModule/Components/settings/settings.component.ts
+++ src/app/SettingsModule/Components/settings/settings.component.ts
@@ -1,20 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { NavbarComponent } from '../../../../shared/navbar/navbar.component';
 import { SidebarComponent } from '../../../../shared/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [NavbarComponent,SidebarComponent],
+  imports: [NavbarComponent, SidebarComponent],
   templateUrl: './settings.component.html',
-  styleUrl: './settings.component.scss'
+  styleUrls: ['./settings.component.scss'] // Note: styleUrl should be style**s**Url
 })
-export class SettingsComponent {
+export class SettingsComponent implements AfterViewInit {
   sidebarVisible: boolean = false;
 
-  constructor() { }
+  @ViewChild('pwHolder') pwHolder!: ElementRef;
+  @ViewChild('resetPw') resetPw!: ElementRef;
 
-  ngOnInit() {
+  constructor() {}
 
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    // Optional: Verify elements are loaded
+    console.log('pwHolder:', this.pwHolder);
+    console.log('resetPw:', this.resetPw);
+  }
+
+  changePwActive() {
+    // Ensure the elements are available before accessing
+    if (this.pwHolder && this.resetPw) {
+      // Set .pw-holder display to none
+      this.pwHolder.nativeElement.style.display = 'none';
+
+      // Set #reset-pw display to flex
+      this.resetPw.nativeElement.style.display = 'flex';
+    } else {
+      console.error('Elements not found');
+    }
   }
 }
